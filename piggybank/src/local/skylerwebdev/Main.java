6c961fdf74cf9ecfafdf262f947ba2c720edec57
skyler dowdy <skylerwebdev@gmail.com>
2019-09-06 11:26:59 -0400

    PBank is holding 7.30

--- piggybank/src/local/skylerwebdev/Main.java
+++ piggybank/src/local/skylerwebdev/Main.java
@@ -7,9 +7,20 @@ public class Main {
 
     myBank = new PiggyBank();
 
-    myBank.addMoney(new Penny(1));
+    myBank.addMoney(new Quarter(1));
+    myBank.addMoney(new Dime(1));
+    myBank.addMoney(new Dollar(5));
+    myBank.addMoney(new Nickel(3));
+    myBank.addMoney(new Dime(7));
+    myBank.addMoney(new Dollar(1));
+    myBank.addMoney(new Penny(10));
 
-    myBank.totalCash();
+
+
+
+
+
+        myBank.totalCash();
     }
 
 }
