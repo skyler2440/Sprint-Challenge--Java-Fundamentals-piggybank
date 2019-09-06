8da67115f80c83a888e75278e79e95f9c14a010b
skyler dowdy <skylerwebdev@gmail.com>
2019-09-06 11:40:22 -0400

    MVP with comments

--- piggybank/src/local/skylerwebdev/Money.java
+++ piggybank/src/local/skylerwebdev/Money.java
@@ -2,17 +2,21 @@ package local.skylerwebdev;
 
 public abstract class Money
 {
+    //set initial value and quantity
     double value;
     int qty;
 
-    public Money(int qty)
+    //setter for quantity
+    Money(int qty)
     {
         this.qty = qty;
     }
 
+    //set / get method for total
     public  abstract double getTotal();
 
-    public int getQty()
+    // getter for quantity
+    int getQty()
     {
         return qty;
     }
